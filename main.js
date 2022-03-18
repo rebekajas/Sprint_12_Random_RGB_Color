48c4bef85df21437f4f8ceac5444fa4b985588a0
rebekajas <rebeka.jas@icloud.com>
2022-03-18 14:23:46 +0200

    second commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let count = 0;
+const clickBtn = document.getElementById("clickBtn");
+const totalClicks = document.getElementById("totalClicks");
+clickBtn.onclick = function () {
+    count++;
+    totalClicks.innerHTML = count;
+
+  
+
+};
+resetBtn.onclick = function () {
+   count = 0;
+   totalClicks.innerHTML = count;
+};
+
